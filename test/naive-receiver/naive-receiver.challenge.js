2818ba4ee84b724061537001068af271ccd732e4
Zach Hoy <hoyzach@gmail.com>
2022-07-07 23:06:51 -0400

    Levels 1-2

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -1,5 +1,5 @@
 const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { expect } = require('chai'); 
 
 describe('[Challenge] Naive receiver', function () {
     let deployer, user, attacker;
@@ -30,7 +30,9 @@ describe('[Challenge] Naive receiver', function () {
     });
 
     it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */   
+        /** CODE YOUR EXPLOIT HERE */
+        const AttackNaiveReceiverFactory = await ethers.getContractFactory('AttackNaiveReceiver', attacker);
+        this.attack = await AttackNaiveReceiverFactory.deploy(this.pool.address, this.receiver.address);
     });
 
     after(async function () {
